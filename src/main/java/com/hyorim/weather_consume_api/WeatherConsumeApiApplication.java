65a7facde2c7dc1db833a8ae60d029f012151184
HyoRim Kang <80194553+hyo-limilimee@users.noreply.github.com>
2025-11-10 21:54:37 +0900

    init: first project setup

---
+++ src/main/java/com/hyorim/weather_consume_api/WeatherConsumeApiApplication.java
@@ -0,0 +1,13 @@
+package com.hyorim.weather_consume_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherConsumeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherConsumeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hyorim/weather_consume_api/WeatherConsumeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyorim.weather_consume_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherConsumeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
