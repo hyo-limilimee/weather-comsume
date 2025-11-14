81af8f1815be6e164656122b6336e6b4c617f381
HyoRim Kang <80194553+hyo-limilimee@users.noreply.github.com>
2025-11-14 23:04:10 +0900

    refactor: change print format

--- src/main/java/com/hyorim/weather_consume_api/domain/WeatherResponse.java
+++ src/main/java/com/hyorim/weather_consume_api/domain/WeatherResponse.java
@@ -2,9 +2,14 @@ package com.hyorim.weather_consume_api.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class WeatherResponse {
     private Double prediction;
+    private String message;
 }
--- src/main/java/com/hyorim/weather_consume_api/service/WeatherService.java
+++ src/main/java/com/hyorim/weather_consume_api/service/WeatherService.java
@@ -7,12 +7,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.text.DecimalFormat;
+
 @Service
 @RequiredArgsConstructor
 public class WeatherService {
 
     private final LogService logService;
-
     private final WebClient webClient;
 
     @Value("${external.fastapi-url}")
@@ -27,7 +28,16 @@ public class WeatherService {
                 .bodyToMono(WeatherResponse.class)
                 .block();
 
-        logService.saveLog(request, response.getPrediction());
+        double rawPrediction = response.getPrediction();
+
+        DecimalFormat df = new DecimalFormat("#,###.##");
+        String formatted = df.format(rawPrediction);
+
+        logService.saveLog(request, rawPrediction);
+
+        String msg = "예상 지하철 승객 수는 약 " + formatted + " 입니다.";
+
+        response.setMessage(msg);
 
         return response;
     }
